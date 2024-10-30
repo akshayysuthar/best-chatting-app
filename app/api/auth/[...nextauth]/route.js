137a30953701fb99a5359eed0d3a9d3fc097eb51
Akshay Suthar <84892497+akshayysuthar@users.noreply.github.com>
2024-10-30 18:00:05 +0530

    Update route.js

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -1,46 +1,28 @@
-import NextAuth from "next-auth";
-import GithubProvider from "next-auth/providers/github";
-import { supabase } from "@/utils/supabase";
+import NextAuth from "next-auth"
+import GithubProvider from "next-auth/providers/github"
+import CredentialsProvider from "next-auth/providers/credentials"
 
-const handler = NextAuth({
+export default NextAuth({
   providers: [
-    // OAuth authentication providers...
     GithubProvider({
       clientId: process.env.GITHUB_ID,
       clientSecret: process.env.GITHUB_SECRET,
-      redirect_uri: `${process.env.NEXTAUTH_URL}/api/auth/callback/github`,
-      authorization: {
-        params: {
-          scope: "read:user,user:email", // Ensure you request the necessary scopes
-        },
+    }),
+    CredentialsProvider({
+      name: "Name and Email",
+      credentials: {
+        name: { label: "Name", type: "text", placeholder: "Your Name" },
+        email: { label: "Email", type: "email", placeholder: "your@email.com" },
+      },
+      async authorize(credentials) {
+        if (credentials.name && credentials.email) {
+          return { id: credentials.email, name: credentials.name, email: credentials.email }
+        }
+        return null
       },
     }),
   ],
-  secret: process.env.NEXTAUTH_SECRET,
-
-  // callbacks: {
-  //   async signIn({ user, account, profile }) {
-  //     const email = profile.email; // Get the email from the profile
-  //     const name = profile.name; // Get the name from the profile
-  //     const avatar = profile.picture; // Get the avatar from the profile
-
-  //     // Check if user already exists in Supabase
-  //     const { data, error } = await supabase
-  //       .from("users")
-  //       .upsert({ email, name, avatar }, { onConflict: ["email"] });
-
-  //     if (error) {
-  //       console.error("Error inserting user into Supabase:", error);
-  //       return false; // Prevent sign-in on error
-  //     }
-  //     return true; // Allow sign-in if no errors
-  //   },
-  //   async session({ session, token }) {
-  //     // Attach user id and other info to the session
-  //     session.user.id = token.id; // This assumes your token has the user ID
-  //     return session;
-  //   },
-  // },
-});
-
-export { handler as GET, handler as POST };
+  pages: {
+    signIn: "/login",
+  },
+})
